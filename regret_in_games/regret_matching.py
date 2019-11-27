f897ff86454c3e3106ed8bfa922b404c283fa270
gota_morishita <morishita_gota@cyberagent.co.jp>
2019-11-27 11:13:35 +0900

    check the input type in Player class

--- regret_in_games/regret_matching.py
+++ regret_in_games/regret_matching.py
@@ -6,8 +6,8 @@ class Player():
     Player class that has a normal game info.
     """
     def __init__(self, normal_game, player):
-        if isinstance(normal_game, (game.OneshotGame)):
-            raise TypeError(f"OneShotGame is only acceptable. YOur input's type is {type(normal_game)}")
+        if isinstance(normal_game, game.OneshotGame):
+            raise TypeError(f"OneShotGame instance is only acceptable. Your input's type is {type(normal_game)}")
 
         if player > normal_game.num_players:
             raise game.ExceedNumPlayersError(f"Your input player exceeds the number of players in the given game. The limit is {normal_game.num_players}, but your input is {player}")
