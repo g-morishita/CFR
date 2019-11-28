650ac7747225d1145912ac071b6de26c67b128fd
gota_morishita <morishita_gota@cyberagent.co.jp>
2019-11-28 16:00:28 +0900

    update the docstring

--- regret_in_games/src/helper_func.py
+++ regret_in_games/src/helper_func.py
@@ -2,5 +2,13 @@ import numpy as np
 import pandas as pd
 
 def check_list_like(input_array):
+    """
+    Check the input is list-like or not.
+
+    Parameters
+    ----------
+    input_array : object
+    """
+
     if not isinstance(input_array, (list, tuple, np.ndarray, pd.DataFrame, pd.Series)):
         raise TypeError(f"input's type should be list, tuple, numpy.ndarray, pd.DataFrame, or pd.Series. Your input: {type(input_array)}")
--- regret_in_games/src/regret_matching.py
+++ regret_in_games/src/regret_matching.py
@@ -4,8 +4,26 @@ import helper_func
 
 class Player():
     """
-    Player class that has a normal game info.
+    Class that has player information.
+
+    Parameters
+    ----------
+    player : int
+        The player in the game.
+
+    Attributes
+    ----------
+    game : normal_game.OneShotGame
+        The game the player participates.
+        It is a class variable.
+    player : int
+        The player in the game.
+    num_actions : int
+        The number of actions the player has.
+    regret_sum : np.ndarray
+        The cumulative regret.
     """
+
     # the game the player play
     game = normal_game.OneShotGame()
 
@@ -17,7 +35,6 @@ class Player():
             self.num_actions = 0
             self.regret_sum = None
 
-
     def initilize_player(self, player):
         if player > self.game.num_players:
             raise normal_game.ExceedNumPlayersError(f"Your input player exceeds the number of players in the given game. "
@@ -27,17 +44,27 @@ class Player():
         self.num_actions = self.game.num_strategies[self.player]
         self.regret_sum = np.zeros(self.num_actions)
 
-
     def init_game(cls, game_matrix):
         """initiate the normal game"""
         cls.game.initialize_game(game_matrix)
 
-
     def simulate_game(self, other_players):
         """
-        simulate the game and return the regret in the simulated game
-        :return: regret (np.ndarray)
+        Simulate the game given as `game`.
+
+        Parameters
+        ----------
+        other_players : np.ndarray
+            The ohter players that participate the game.
+
+        Returns
+        -------
+        resultant_utilities : np.ndarray
+            The utilities that each player gains after playing the game.
+        played_actions : np.ndarray
+            The actions that each player actually played in the game.
         """
+
         # check if other players are actually players
         if not np.array([isinstance(player, Player) for player in other_players]).all():
             raise NotPlayerError("The inputs are inavlid. The acceptable type is only Player.")
@@ -45,15 +72,20 @@ class Player():
         all_players = other_players + [self]
         all_players.sort(key=lambda player: player.player)
         mixed_strategies = [player.regret_to_strategy() for player in all_players]
-        gained_utilities, played_pure_strategies = self.game.play_prob(mixed_strategies)
-        return gained_utilities, played_pure_strategies
-
+        resultant_utilities, played_actions = self.game.play_prob(mixed_strategies)
+        return resultant_utilities, played_actions
 
     def regret_to_strategy(self):
         """
-        transform the cumulative regret to a mixed strategy
-        :return: mixed strategy (np.ndarray)
+        Transform the cumulative regret into the mixed strategy.
+        If the cumulative regret is all 0, the mixed strategy is uniformly random.
+
+        Returns
+        -------
+        mixed_strategy : np.ndarray
+            The mixed strategy.
         """
+
         # if the cumulative regret is 0, his/her mixed strategy is uniformly random.
         # Note that the initial mixed strategy does not need to be uniformly random.
         if (self.regret_sum == 0).all():
@@ -62,25 +94,24 @@ class Player():
             mixed_strategy = self.regret_sum / self.regret_sum.sum()
         return mixed_strategy
 
-
-    def _calc_regrets(self, played_pure_strategies):
+    def _calc_regrets(self, played_actions):
+        """Calculate the regret according to actions that are played."""
         regrets = []
         add_regret = regrets.append
-        helper_func.check_list_like(played_pure_strategies)
-        base_utility = self.game.play_pure_strategy(played_pure_strategies)[self.player]
+        helper_func.check_list_like(played_actions)
+        base_utility = self.game.play_pure_strategy(played_actions)[self.player]
         for action in range(self.num_actions):
-            played_pure_strategies[self.player] = action
-            regret = self.game.play_pure_strategy(played_pure_strategies)[self.player] - base_utility
+            played_actions[self.player] = action
+            regret = self.game.play_pure_strategy(played_actions)[self.player] - base_utility
             add_regret(regret)
 
         return np.array(regrets)
 
-
     def update_cum_regret(self, played_pure_strategies):
+        """Update the cumulative regret. If regret is negative, it is reduced to 0."""
         regrets = self._calc_regrets(played_pure_strategies)
         regrets[regrets < 0] = 0
         self.regret_sum += regrets
-
 
 class NotPlayerError(Exception):
     def __init__(self, message):
